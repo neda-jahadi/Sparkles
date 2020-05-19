4e689835942fb303657f838190f1b62e852011fc
Hanna Möllergren <hannamollergren@gmail.com>
2020-05-19 14:54:25 +0200

    startcard visar bara fem senaste list items, slice

--- src/components/StartCard.jsx
+++ src/components/StartCard.jsx
@@ -11,23 +11,25 @@ const StartCard = () => {
 		lista = <p className="text-red">No list items</p> 
 	}
 	else{
-		lista = list.map(item => (
+		let fiveLatestItems = list.slice(Math.max(list.length - 5, 0))
+		lista = fiveLatestItems.map(item => (
 		<p key={item.title} className="list-item text-red">{item.title} - {item.creator}</p>	
 		))
 	}
 	
-
 	return(
 		<div>
 			<div className="start-card background-red">
-				<img src={Music} alt="Icon" width="36px" height="51px" className="icon"/>
-				<h2 className="title text-red">Music</h2>
+				<div className="title-container">
+					<h2 className="title text-red">Music</h2>
+					<img src={Music} alt="Icon" width="26px" height="41px" className="icon"/>
+				</div>
+			
 				{lista}
 				<div className="btn-container">
 					<button className="btn-list">Music</button>
 					<button className="btn-add">Add music</button>
-				</div>
-				
+				</div>		
 			</div>
 		</div>
 		
