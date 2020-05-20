4bbc2a34fd470c9203d2b55e13b131f1370aa188
Karin Karlsson <karin.drake@gmail.com>
2020-05-20 14:39:54 +0200

    påbörjat koppla kategori till css i listcard

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import './ListCardStyle.css'
 import Edit from '../assets/edit.png'
+import { useSelector } from 'react-redux';
 
 
 const ListCard=({title, creator, usedBefore, rating, comment})=>{
+    
+   //const dispatch
+    const category=useSelector(state=>state.category);
+   
+    let creatorText='', usedBeforeText='', colorClass='';
+ 
+    
+    switch (category){
 
+        case 'music':
+            creatorText='Artist';
+            usedBeforeText='Listened to';
+            colorClass='red'
+            break;
+
+        case 'books':
+            creatorText='Author';
+            usedBeforeText='Read'; 
+            break;
+
+        case 'movies':
+            creatorText='Producer';
+            usedBeforeText='Seen';
+            break;
+
+        default:
+        console.log('Oklart vad jag ska ha i default')
+
+
+
+    }
 
     return (
 
-        <div className='list-card'>
+        <div className={'list-card '+colorClass}>
             <h3>{title}</h3>
 
-            <p>Creator is: {creator}</p>
-            <p>Listened to: {usedBefore}s</p>
+            <p>{creatorText}: {creator}</p>
+            <p>{usedBeforeText}: {usedBefore}s</p>
             <p>Rating: {rating} of 5</p>
             <p>Comment: {comment} </p>
             
